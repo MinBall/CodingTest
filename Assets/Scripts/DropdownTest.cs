65941df99516c5539a2437c3aafc8be853a92b38
MinBall <pymmin0403@naver.com>
2023-04-17 11:29:56 +0900

    Dropdown Setting
    언어 변경을 드롭다운으로 바꾸었습니다.

---
+++ Assets/Scripts/DropdownTest.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DropdownTest : MonoBehaviour
+{
+    string DROPDOWN_KEY = "DROPDOWN_KEY";
+
+    int currentOption;
+    public static TMP_Dropdown options;
+
+    List<string> optionList = new List<string>();
+
+    LocalizationManager localizationManager;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey(DROPDOWN_KEY) == false) currentOption = 0;
+        else currentOption = PlayerPrefs.GetInt(DROPDOWN_KEY);
+    }
+
+    void Start()
+    {
+        options = this.GetComponent<TMP_Dropdown>();
+
+        options.ClearOptions();
+
+        optionList.Add("Korean");
+        optionList.Add("English");
+        optionList.Add("Hungarian");
+        optionList.Add("Ukrainian");
+
+        options.AddOptions(optionList);
+
+        options.value = currentOption;
+
+        options.onValueChanged.AddListener(delegate { setDropDown(options.value); });
+        setDropDown(currentOption); //최초 옵션 실행이 필요한 경우
+    }
+
+    void setDropDown(int option)
+    {
+        PlayerPrefs.SetInt(DROPDOWN_KEY, option);
+        LocalizationManager.DropdownPlay();
+
+        // option 관련 동작
+        Debug.Log("current option : " + options.value);
+    }
+}
+
